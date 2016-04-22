6c773261a761cc6f7a54c9e93e6c20190bfebbd3
Vinícius <silvcamposvinicius@gmail.com>
2016-04-22 17:41:55 -0300

    Netbeans Project

---
+++ SD/processo.h
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   processo.h
+ * Author: vinicius
+ *
+ * Created on April 22, 2016, 5:31 PM
+ */
+
+#ifndef PROCESSO_H
+#define PROCESSO_H
+
+class processo {
+public:
+    processo();    
+    virtual ~processo();
+    void set_pid(int id);
+    int get_pid();
+    void signal_handler(int code);
+    void send_signal(int pid, int sign);
+    void receive_signal();
+private:
+    int pid;
+    bool busyWait;
+
+};
+
+#endif /* PROCESSO_H */
+
---
+++ SD/sinais.cpp
@@ -0,0 +1,132 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+#include <csignal> //signal, kill
+#include <iostream> //cout, cin
+#include <stdlib.h> //atoi (string para inteiro), comando "system"
+#include <string> //strings, getline
+#include <sstream> //criar sstream (para converter string p int)
+#include <unistd.h> //fork
+
+using namespace std;
+
+void signal_handler(int code){
+	//modifiquei o tipo de retorno para bool para aproveitar o comportamento em demais locais
+	//adicionei o case 17 e o default (erro de digitação)	
+
+	bool wrongCode = false;
+
+	switch(code){
+		
+
+		case 8: //SIGFPE: floating point exception
+			//fazer algo
+			cout << "Case 8" << endl;
+			break;
+
+		case 10: //SIGUSR1: user-defined 1
+			//fazer algo
+			break;
+
+		case 12: //SIGUSR2: user-defined 2
+			//fazer algo
+			break;
+
+		case 17:  //SIGCHLD: Parada do processo filho
+			//fazer algo
+			break;
+
+		default:
+			cout<< "Código inválido, tente novamente." <<endl;
+			wrongCode = true;	
+
+	}
+
+}
+
+
+int main(int argc, char* argv) {
+	
+	int child = fork(); //cria processo filho, retorna pid do filho para o pai e 0 para o filho
+	//Sinal para encerrar o processo filho: SIGCHLD (17)
+
+	//Emissor
+	
+	int signalCode = 0;
+
+	if (child > 0){
+		
+		string temp;		
+		bool wrongCode = true;
+
+		while(wrongCode) {		
+
+			cout << "Insira um código de sinal. Opções:" << endl;
+			cout << "-(Digite 8) SIGFPE (Floating Point Exception;" << endl;
+			cout << "-(Digite 10) SIGUSR1 (User-Defined Signal 1);" << endl;
+			cout << "-(Digite 12) SIGUSR2 (User-Defined Signal 2);" << endl;
+			cout << "-(Digite 17) SIGCHLD (Parada de processo filho);" << endl;
+
+
+
+			getline(cin, temp);
+
+			signalCode = atoi(temp);
+
+			wrongCode = signal_handler(signalCode);
+
+		}
+
+
+		kill(child, signalCode); //enviar sinal de código signalCode para filho
+
+	}
+
+	//Receptor
+	
+
+	else if (child == 0) {
+		
+
+		void (*handler1)(int);
+		
+		handler1 = signal(SIGFPE, signal_handler);
+
+
+		void (*handler2)(int);
+		
+		handler2 = signal(SIGUSR1, signal_handler);
+
+
+		void (*handler3)(int);
+		
+		handler3 = signal(SIGUSR2, signal_handler);
+
+		
+		void (*handler4)(int);
+		
+		handler4 = signal(SIGCHLD, signal_handler);
+		
+		while (true) {
+
+			int i = 1;
+
+		}
+
+
+
+	}
+
+	else { //child == -1 significa que houve um erro
+
+		cout << "Ocorreu um erro na criação do processo filho. Tente novamente." << endl;
+
+	}
+
+	return 0;
+
+}
