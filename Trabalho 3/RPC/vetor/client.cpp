e7d00ed1e725838be8dc78da7d25d4a4f3a8895e
Lucas <lucas.gomes@poli.ufrj.br>
2016-07-03 18:04:10 -0300

    (RPC) Servidor agora atende a várias requisições simultaneamente

--- Trabalho 3/RPC/vetor/client.cpp
+++ Trabalho 3/RPC/vetor/client.cpp
@@ -18,10 +18,24 @@ using namespace apache::thrift::transport;
 using namespace oparitmeticas;
 
 int main() {
+  
+  
+/*
+
   boost::shared_ptr<TTransport> socket(new TSocket("localhost", 9090));
   boost::shared_ptr<TTransport> transport(new TBufferedTransport(socket));
   boost::shared_ptr<TProtocol> protocol(new TBinaryProtocol(transport));
   CalculatorClient client(protocol);
+
+*/
+
+
+  boost::shared_ptr<TSocket> socket(new TSocket("localhost", 9090));
+  boost::shared_ptr<TTransport> transport(new TFramedTransport(socket));
+  boost::shared_ptr<TProtocol> protocol(new TBinaryProtocol(transport));
+
+  CalculatorClient client(protocol);
+
     
   try {
     transport->open();
--- Trabalho 3/RPC/vetor/server.cpp
+++ Trabalho 3/RPC/vetor/server.cpp
@@ -1,21 +1,34 @@
 #include "Calculator.h"
 #include <thrift/protocol/TBinaryProtocol.h>
-#include <thrift/server/TSimpleServer.h>
+//#include <thrift/server/TSimpleServer.h>
 #include <thrift/transport/TServerSocket.h>
 #include <thrift/transport/TBufferTransports.h>
+
+
+//multithreaded server
+#include <thrift/concurrency/ThreadManager.h>
+#include <thrift/concurrency/PosixThreadFactory.h>
+#include <thrift/server/TNonblockingServer.h>
 
 #include <vector> //std::vector
 #include <cmath> //std::pow, std::sqrt, std::log
 #include <iostream> //std::cout, std::endl
-#include <thread>  //std::thread
-#include <atomic> //std::atomic_flag
+
+
+#include <unistd.h> //sleep para testes
 
 using namespace ::apache::thrift;
+
 using namespace ::apache::thrift::protocol;
 using namespace ::apache::thrift::transport;
 using namespace ::apache::thrift::server;
+using namespace ::apache::thrift::concurrency;
+
 using boost::shared_ptr;
+
+
 using namespace oparitmeticas;
+
 
 
 class CalculatorHandler : virtual public CalculatorIf {
@@ -36,6 +49,10 @@ class CalculatorHandler : virtual public CalculatorIf {
     }
 
     std::cout << "power" << std::endl;
+
+    // REMOVER!!!!
+
+    sleep(4);
   }
 
   void log(std::vector<double> & _return, const std::vector<double> & received) {
@@ -60,6 +77,9 @@ class CalculatorHandler : virtual public CalculatorIf {
 
 int main(int argc, char **argv) {
   int port = 9090;
+  
+  /*
+
   shared_ptr<CalculatorHandler> handler(new CalculatorHandler());
   shared_ptr<TProcessor> processor(new CalculatorProcessor(handler));
   shared_ptr<TServerTransport> serverTransport(new TServerSocket(port));
@@ -67,6 +87,22 @@ int main(int argc, char **argv) {
   shared_ptr<TProtocolFactory> protocolFactory(new TBinaryProtocolFactory());
 
   TSimpleServer server(processor, serverTransport, transportFactory, protocolFactory);
+
+  */
+
+
+  // using thread pool with maximum 15 threads to handle incoming requests
+  shared_ptr<CalculatorHandler> handler(new CalculatorHandler());
+  shared_ptr<TProcessor> processor(new CalculatorProcessor(handler));
+  shared_ptr<TProtocolFactory> protocolFactory(new TBinaryProtocolFactory());
+
+  shared_ptr<ThreadManager> threadManager = ThreadManager::newSimpleThreadManager(128);
+  shared_ptr<PosixThreadFactory> threadFactory = shared_ptr<PosixThreadFactory>(new PosixThreadFactory());
+  threadManager->threadFactory(threadFactory);
+  threadManager->start();
+  TNonblockingServer server(processor, protocolFactory, port, threadManager);
+
+  
   server.serve();
   return 0;
 }
