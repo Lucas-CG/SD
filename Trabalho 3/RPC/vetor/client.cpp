bac333fdf88e23dce23f5ed60ea83f75aa24f3ed
viniciusscampos <silvcamposvinicius@gmail.com>
2016-07-06 16:14:13 -0300

    Inicio de implementação do client com threads.

--- Trabalho 3/RPC/vetor/client.cpp
+++ Trabalho 3/RPC/vetor/client.cpp
@@ -9,6 +9,7 @@
 #include <vector> //std::vector
 #include <cmath> //std::pow, std::sqrt, std::log
 #include <iostream> //std::cout, std::endl
+#include <thread>
 
 using namespace std;
 using namespace apache::thrift;
@@ -17,35 +18,57 @@ using namespace apache::thrift::transport;
 
 using namespace oparitmeticas;
 
-int main() {
-  
-  
-/*
 
-  boost::shared_ptr<TTransport> socket(new TSocket("localhost", 9090));
-  boost::shared_ptr<TTransport> transport(new TBufferedTransport(socket));
-  boost::shared_ptr<TProtocol> protocol(new TBinaryProtocol(transport));
-  CalculatorClient client(protocol);
+void getPower(std::vector<double> &vec, int begin, int end){
+  std::vector<double> v (vec.begin() + begin,  vec.end() - end);  
+  for(auto& value : v){
+    std::cout << value << std::endl;
+  }
+}
 
-*/
+int main(int argc, char** argv) {
 
+  //boost::shared_ptr<TSocket> socket(new TSocket("localhost", 9090));
+  //boost::shared_ptr<TTransport> transport(new TFramedTransport(socket));
+  //boost::shared_ptr<TProtocol> protocol(new TBinaryProtocol(transport));
 
-  boost::shared_ptr<TSocket> socket(new TSocket("localhost", 9090));
-  boost::shared_ptr<TTransport> transport(new TFramedTransport(socket));
-  boost::shared_ptr<TProtocol> protocol(new TBinaryProtocol(transport));
+  //CalculatorClient client(protocol);
 
-  CalculatorClient client(protocol);
+  static const double lt[] = {2.0, 3.0, 5.0,6.0};
+  int sizeList = sizeof(lt)/sizeof(lt[0]);
+  std::vector<double> vec (lt, lt + sizeList );
+  std::vector<std::thread> threads;
+  int numThreads = 2;
+  int partSize = sizeList/numThreads;
 
-    
+  for (int i = 0; i < numThreads; i++) {   
+    threads.push_back( std::thread(getPower,std::ref(vec), i*partSize, i*partSize) );
+  }
+
+  for(auto& th : threads) th.join();
+
+  /*
   try {
+    
     transport->open();
 
     client.ping();
     cout << "ping()" << endl;
 
-    static const double lt[] = {2.0, 3.0, 5.0};
-    std::vector<double> vec (lt, lt + sizeof(lt) / sizeof(lt[0]) );
+    static const double lt[] = {2.0, 3.0, 5.0,6.0};
+    long int sizeList = sizeof(lt)/sizeof(lt[0]);
+    std::vector<double> vec (lt, lt + sizeList );
+    std::vector<std::thread> threads;
+    int numThreads = 1;    
+    long int partSize = sizeList/numThreads;
+    //std::cout<<partSize<<std::endl;
 
+    for(int i=0; i<numThreads; i++){
+      threads.push_back(std::thread(getPower,std::ref(vec),i*partSize,(numThreads-i)*partSize));
+    }
+    //getPower(vec,1,3);
+
+    /*
     try {
       client.power(vec, vec, 2);
       for (auto & it : vec) {
@@ -55,7 +78,10 @@ int main() {
       cout << "InvalidOperation: " << io.why << endl;
     }
     transport->close();
+  
+    transport -> close();
   } catch (TException& tx) {
     cout << "ERROR: " << tx.what() << endl;
   }
+  */
 }
